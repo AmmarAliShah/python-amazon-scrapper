0de0d3be20d04a03da25af577bb9864aa8d1c904
Ammar Shah <syedammarali24@gmail.com>
2023-12-29 10:08:09 +0500

    Books To Scrape completed

--- books-toscrape.py
+++ books-toscrape.py
@@ -5,8 +5,10 @@ import pandas as pd
 # Global Variables
 current_page = 1
 proceed = True
+data = []
 
 while(proceed):
+    print("Currently on page " + str(current_page))
     url = "https://books.toscrape.com/catalogue/page-" + str(current_page)+  ".html"
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
@@ -20,5 +22,13 @@ while(proceed):
             item = {}
 
             item['Title'] = book.find("img").attrs["alt"]
-            item['Link'] = book.find()
+            item['Link'] = book.find("a").attrs["href"]
+            item['Price'] = book.find("p", class_="price_color").text[1:]
+            item['Stock'] = book.find("p", class_="instock availability").text.strip()
+
+            data.append(item);
+
     current_page += 1
+
+df = pd.DataFrame(data)
+df.to_excel("books.xlsx")
